e12a4a08883915b7596b3c9ac7f65b85d1a33f65
hana halouani <hanahalouani88@gmail.com>
2023-10-27 11:10:17 +0200

    Update

---
+++ DataAccesslibrairy/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccesslibrairy;
+
+public interface ISqlDataAccess
+{
+    Task<List<T>> LoadData<T, U>(string sql, U parameters);
+    Task SaveData<T, U>(string sql, U parameters);
+}
---
+++ DataAccesslibrairy/Models/PeopleModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccesslibrairy.Models
+{
+    public class PeopleModel
+    {
+        public int? Id { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "Name is too short")]
+        [MinLength(5, ErrorMessage = "name is too short")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "LastName is too short")]
+        [MinLength(5, ErrorMessage = "Lastname is too short")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public DateTime DateHire { get; set; }
+    }
+}
